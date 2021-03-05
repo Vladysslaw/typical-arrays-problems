5ad1a5bc148117babc0cfc19dc5023a51a63f51e
Vlad <vladuha011@mail.ru>
2021-03-05 19:43:35 +0300

    init: start typical-array-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,51 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if (array == undefined) {
+    return 0;
+  }
+  if (array.length == 0) {
+    return 0;
+  }
+  let min = array[0];
+  array.forEach(item => {
+    if (item < min) {
+      min = item;
+    }
+  });
+  
+  return min;
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+  if (array == undefined) {
+    return 0;
+  }
+  if (array.length == 0) {
+    return 0;
+  }
+  let max = array[0];
+  array.forEach(item => {
+    if (item > max) {
+      max = item;
+    }
+  });
+  
+  return max;
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  if (array == undefined) {
+    return 0;
+  }
+  if (array.length == 0) {
+    return 0;
+  }
+  let sum = 0;
+  let i = 0;
+  array.forEach(item => {
+    i++;
+    sum += item;
+  });
+  let avg = sum / i;
+  return avg;
+};
